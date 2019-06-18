69a755f0296e7b0c0f80fda207ec9c28d57554c6
fariezky55 <48426988+fariezky55@users.noreply.github.com>
2019-06-18 14:43:35 +0700

    ini file php

---
+++ Untitled-1.php
@@ -0,0 +1,17 @@
+<?php
+$a=15;
+$b=8;
+ 
+if ($a > $b) 
+{
+    echo "a lebih besar daripada b";
+} 
+elseif ($a == $b) 
+{
+    echo "a sama besar dengan b";
+} 
+else
+{
+    echo "a lebih kecil daripada b";
+}
+?>
